b668cfef32c18b80ab9b9d7e8917f6a9d5f453ce
Gerard Gold <ggtechguru@gmail.com>
2018-03-25 17:29:39 -0700

    Frames & click

---
+++ Selenium/SeleniumJavaOnline/SeleniumJavaOnline_A/src/frames_a/FramesA.java
@@ -0,0 +1,34 @@
+
+package frames_a;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class FramesA {
+    
+	public static void main(String[] argv)	{
+		
+		System.setProperty(
+				"webdriver.chrome.driver", 
+				"/home/croesus/node_modules/chromedriver/lib/chromedriver/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();	
+
+                driver.get("http://demo.guru99.com/selenium/deprecated.html");
+                
+                driver.switchTo().frame("classFrame");
+                
+                driver.findElement(By.linkText("Deprecated")).click();
+                
+		try	{
+			TimeUnit.SECONDS.sleep(5);
+		}
+		catch (Exception e) {}
+	
+		driver.quit();
+	
+	}
+}
