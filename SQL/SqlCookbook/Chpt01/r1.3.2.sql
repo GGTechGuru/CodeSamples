7f5ea79ab2dabff9665350c775e1a0fb1c8dc2ec
Gerard Gold <ggtechguru@gmail.com>
2018-03-31 19:07:59 -0700

    Stuff

---
+++ SQL/SqlCookbook/Chpt01/r1.3.2.sql
@@ -0,0 +1,8 @@
+-- Finding rows that satisfy multiple conditions, using parens
+select *
+  from emp
+where (	deptno = 10
+  		or comm is not null
+  		or sal <= 2000
+	)
+ and deptno=20
---
+++ SQL/SqlCookbook/Chpt01/r1.3.sql
@@ -0,0 +1,6 @@
+-- Finding rows that satisfy multiple conditions 
+select *
+  from emp
+where deptno = 10
+  or comm is not null
+  or sal <= 2000 and deptno=20
